82ce5ebe1ce0c78cc48b5349c79619cdff119c2d
poshkent <poshkent@users.noreply.github.com>
2021-12-02 13:12:11 +0300

    v2
    added all people, new funtional duty

--- teg_bot.py
+++ teg_bot.py
@@ -1,24 +1,73 @@
 from ast import parse
 import telebot
-import os
 import time
 import schedule
 from datetime import datetime
 from multiprocessing import Process
 
-from telebot.types import Message, Venue
 
 
-all1 = {'Дима': '752872267', 'Паша': '629075242', 'Полина Кочеткова': '967925905', 'Виталик': '485433810', 'Витя': '1250087021', 'Полина Ковалева': '650761348',
-        'Леша': '751974761', 'Ваня': '701727602', 'Наташа': '461883085', 'Максим': '688659984', 'Антон': '1084801379', 'Егор Белявский': '1280128315', 'Никита': '1242174386'}
-hostel = {'Паша': '629075242', 'Полина Кочеткова': '967925905', 'Дима': '752872267', 'Ваня': '701727602',
-          'Наташа': '461883085', 'Максим': '688659984', 'Антон': '1084801379', 'Егор Белявский': '1280128315', 'Никита': '1242174386'}
-minsk = {'Егор Белявский': '1280128315', 'Виталик': '485433810',
-         'Леша': '751974761', 'Антон': '1084801379'}
+all = {
+    'Ceвa': '1021465762',
+    'Антон': '1084801379',
+    'Артём': '985490261',
+    'Ваня': '701727602',
+    'Виталик': '485433810',
+    'Дима Борейко': '752872267',
+    'Егор Белявский': '1280128315',
+    'Егор Дробушевич': '1147728466',
+    'Егор Крылов': '1986086510',
+    'Егор Панковец': '790220005',
+    'Леша': '751974761',
+    'Максим': '688659984',
+    'Негр': '1250087021',
+    'Никита Белостоцкий': '1242174386',
+    'Паша': '629075242',
+    'Полина Ковалева': '650761348',
+    'Полина Кочеткова': '967925905',
+    'Помело': '461883085',
+    'Саша Бренчанинов' : '1318315130',
+    'Серёжа': '994027544',
+    'Дима Василевич': '1836814972',
+    'Женя': '1010820388',
+    'Саша Габриневский' : '503086754',
+    'Дед' : '927086053',
+    'Денис' : '1123009924'
+}
+
+hostel = [
+    'Ceвa',
+    'Артём',
+    'Ваня',
+    'Дима Борейко',
+    'Максим',
+    'Негр',
+    'Никита Белостоцкий',
+    'Паша',
+    'Полина Ковалева',
+    'Полина Кочеткова',
+    'Помело',
+    'Саша Бренчанинов',
+    'Серёжа',
+]
+
+minsk = [
+    'Дед',
+    'Саша Габриневский',
+    'Женя',
+    'Дима Василевич',
+    'Егор Панковец',
+    'Егор Крылов',
+    'Егор Дробушевич',
+    'Антон',
+    'Виталик',
+    'Егор Белявский',
+    'Леша',
+    'Денис',
+]
+
 TOKEN = '1827746031:AAGKqa_5XyWhcBYtCIZ_QxhcQdmaoIHcHWQ'
 CHAT_ID = '-1001185015555'
-duty = ['@againTL @Tomasttt @greedann', '@Shuhmen @NiK1TA315',
-        '@kowalskivarianty ', '@natarg @Fllloyd', '@Appolinapiya ', ]
 bot = telebot.TeleBot(TOKEN)
 
 
@@ -39,6 +88,21 @@ class Nahui:
         self.date = '{} {}'.format(self.line[-6:-4], self.line[-3:-1])
         self.message = self.line[0:-8]
 
+    def notify(a = None):
+        now = datetime.now()
+        date = now.strftime("%d %m")
+        try:
+            students = stat.get_people(date)
+        except:
+            bot.send_message(CHAT_ID, 'Дежурных нет идите нахуй', parse_mode="Markdown")
+        students = list(students.split(', '))
+        tag = 'Дежурные на неделю: '
+        for piple in students:
+            tag += f'[{piple}](tg://user?id={all[piple]}), '
+        tag = tag[:-2]
+        bot.send_message(CHAT_ID, tag, parse_mode="Markdown")
+        
+
     def collect(self):
         with open(self.file_name, 'r', encoding='utf-8') as file:
             for line in file:
@@ -49,51 +113,39 @@ class Nahui:
         return self.stat[date]
 
 
-def notify():
-    now = datetime.now()
-    date = now.strftime("%d %m")
-    try:
-        students = stat.get_people(date)
-        message = f"Дежурные на неделю: {students}"
-        bot.send_message(CHAT_ID, message, parse_mode="Markdown")
-    except:
-        bot.send_message(CHAT_ID, 'Дежурных нет идите нахуй',
-                         parse_mode="Markdown")
-
 
 @bot.message_handler(commands=['all'])
 def tag_all(message):
     tag = ""
-    for key, value in all1.items():
-        tag += f'[{key}](tg://user?id={value}) '
+    for key, value in all.items():
+        tag += f'[{key}](tg://user?id={value}), '
     bot.send_message(message.chat.id, tag, parse_mode="Markdown")
 
 
 @bot.message_handler(commands=['hostel'])
 def tag_hostel(message):
     tag = ""
-    for key, value in hostel.items():
-        tag += f'[{key}](tg://user?id={value}) '
+    for key in hostel:
+        tag += f'[{key}](tg://user?id={all[key]}) '
     bot.send_message(message.chat.id, tag, parse_mode="Markdown")
 
 
 @bot.message_handler(commands=['minsk'])
 def tag_minsk(message):
     tag = ""
-    for key, value in minsk.items():
-        tag += f'[{key}](tg://user?id={value}) '
+    for key in minsk:
+        tag += f'[{key}](tg://user?id={all[key]}) '
     bot.send_message(message.chat.id, tag, parse_mode="Markdown")
 
 
 @bot.message_handler(commands=['duty'])
 def duty(message):
-    tag_duty = '[Полина](tg://user?id=967925905)'
-    bot.send_message(message.chat.id, tag_duty, parse_mode="Markdown")
+    stat.notify()
 
 
-stat = Nahui('list.txt')
+stat = Nahui('d:\Desktop\list.txt')
 stat.collect()
-schedule.every().sunday.at("22:00").do(notify)
+schedule.every().sunday.at("22:00").do(stat.notify)
 
 
 def loop():
